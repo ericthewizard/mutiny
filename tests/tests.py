ce0f2020125ce25d4b9b54a9232c50af838fa82a
supervised <abused@gmail.com>
2024-12-16 17:30:07 -0500

    oops

--- tests/tests.py
+++ tests/tests.py
@@ -132,6 +132,3 @@ def test_nans_log_scale():
     options('test_spec_nan', 'zlog', True)
     tplot('test_spec_nan', display=False, save_png=current_directory + 'nans_zlog_regression')
 
-
-if __name__ == '__main__':
-    unittest.main()
